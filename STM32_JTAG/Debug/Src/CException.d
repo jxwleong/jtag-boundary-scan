fd667ed2e96c5800404e1fa9a54c54575b6fd316
jason9829 <jason9829@live.com>
2019-08-16 16:48:27 +0800

    Removed all artifacts

--- STM32_JTAG/Debug/Src/CException.d
+++
@@ -1,4 +0,0 @@
-Src/CException.o: ../Src/CException.c \
- C:/student/JLXW/JTAG_BoundaryScan/STM32_JTAG/Inc/CException.h
-
-C:/student/JLXW/JTAG_BoundaryScan/STM32_JTAG/Inc/CException.h:
--- STM32_JTAG/Debug/Src/TAP_Mock.d
+++
@@ -1,7 +0,0 @@
-Src/TAP_Mock.o: ../Src/TAP_Mock.c \
- C:/student/JLXW/JTAG_BoundaryScan/STM32_JTAG/Inc/TAP_Mock.h \
- C:/student/JLXW/JTAG_BoundaryScan/STM32_JTAG/Inc/TAP.h
-
-C:/student/JLXW/JTAG_BoundaryScan/STM32_JTAG/Inc/TAP_Mock.h:
-
-C:/student/JLXW/JTAG_BoundaryScan/STM32_JTAG/Inc/TAP.h:
